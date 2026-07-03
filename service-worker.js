3458316dfbb45ddf4be68d150a618a283f02d9f8
ghostbuilder <yoannpetit011@gmail.com>
2026-07-03 16:09:05 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME='journal-chantier-sncf-v20';
-const APP_SHELL=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('supabase.co')||e.request.url.includes('cdn.jsdelivr.net')){e.respondWith(fetch(e.request));return}
-  if(e.request.mode==='navigate'){e.respondWith(fetch(e.request).catch(()=>caches.match('./index.html')));return}
-  e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))
-});
